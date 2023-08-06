e17c6976be2f9fd28e0c5baf6dffc46684a87fbb
MediumDifficulty <tduffy468@gmail.com>
2023-08-06 01:05:26 +0100

    Fixed wearing elytra check

--- src/client/java/yes/mediumdifficulty/elytratime/ElytraTimeClient.java
+++ src/client/java/yes/mediumdifficulty/elytratime/ElytraTimeClient.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ElytraItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -31,7 +32,7 @@ public class ElytraTimeClient implements ClientModInitializer {
             if (printTime.wasPressed() && client.player != null) {
                 ItemStack chestPlate = client.player.getInventory().getArmorStack(EquipmentSlot.CHEST.getEntitySlotId());
 
-                if (!chestPlate.isEmpty()) {
+                if (chestPlate.getItem() instanceof ElytraItem) {
                     client.player.sendMessage(Text.translatable("message.elytratime.time_report", Calculator.formatTime(Calculator.timeLeft(chestPlate)))
                             .formatted(Formatting.GREEN));
                 } else {
